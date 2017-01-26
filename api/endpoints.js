9bdb94123d4e949a60cb4ef24f3403f3788d5b95
Vishal Lal <vishal.lal@container-solutions.com>
2017-01-26 15:42:05 -0800

    Explicity set domian

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,6 +1,17 @@
 (function (){
   'use strict';
 
+  var domain = "";
+  process.argv.forEach(function (val, index, array) {
+    var arg = val.split("=");
+    if (arg.length > 1) {
+      if (arg[0] == "--domain") {
+        domain = arg[1];
+        console.log("Setting domain to:", domain);
+      }
+    }
+  });
+
   module.exports = {
     catalogueUrl:  "http://catalogue" + domain,
     tagsUrl:       "http://catalogue" + domain + "/tags",
